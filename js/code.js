7c7b58e32fe0bc19e5060fac0e868761aa9e809f
Sebastian Reimann <reimannseba@gmail.com>
2022-07-27 16:14:52 -0400

    add js

---
+++ js/code.js
@@ -0,0 +1,14 @@
+console.log("hola")
+
+// const $form = document.querySelector ("#form")
+// const $mailto = document.querySelector('#mail')
+
+// $form.addEventListener ('submit', enviarMsj);
+
+// function enviarMsj(event) {
+//     event.preventDefault()
+//     const form = new FormData(this)
+//     $mailto.setAttribute("href", 'mailto:reimannseba@gmail.com?subject=nombre {form.get("names")} email ${form.get("email")} &body=${form.get("country")} ${form.get("web")} ${form.get("msje")}')
+//     $mailto.click()
+
+// }
